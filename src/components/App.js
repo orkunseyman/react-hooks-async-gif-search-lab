fd640df814805dc9567f37f4fd574a43f3dabd25
orkunseyman <orkunseyman@gmail.com>
2022-04-01 00:48:38 +0300

    finish asssigment

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,7 @@ import React from "react";
 
 import NavBar from "./NavBar";
 
-// the App component should render out the GifListContainer component
-
+import GifListContainer from "./GifListContainer";
 function App() {
   return (
     <div>
